9fe573c365ad16de9fe8b6a8156dd2d38813ac0a
Sujit901 <sharmasujit901@gmail.com>
2021-03-15 09:22:32 +0545

    My First Git Project

---
+++ func.py
@@ -0,0 +1,48 @@
+# def add_two_numbers(num1,num2,num3=0):
+#     s = num1+num2+num3
+#    return s
+
+# ss = add_two_numbers(3,4)
+# print("Sum is",ss)
+
+
+
+# def add_two_numbers(*args):
+#     print(type(args))
+#     print(args)
+#     s = 0
+#     for i in args:
+#         s = s + i
+#     return s
+
+# ss = add_two_numbers(3,4)
+# print("Sum is",ss)
+
+
+# def add_two_numbers(*args):
+#     print(type(args))
+#     print(args)
+#     s = sum(args)
+#     return s
+
+# ss = add_two_numbers(3,4)
+# print("Sum is",ss)
+
+
+# def information(**kwargs):
+#     print(type(kwargs),kwargs)
+#     for k, v in kwargs.items():
+#         print(k, v)
+
+# information(name="Ram", age=25, address="Bhaktapur")
+
+
+# d = "Sujit"
+# a = 10
+# print("Difference of two is: ",d-a) #(Type error example)
+
+
+#x = [5, 4, 3, 2, 1]
+#y = sorted(x)
+#y = x.sort()
+#print(x, y)
---
+++ hello.py
@@ -0,0 +1,8 @@
+print("Hello")
+
+name = input("What is your name")
+
+print(type(name)) #str
+print(name)
+d = {"name" : "Sujit","age" : 20,"address" : "ktm"}
+for k,v in d.items(): print(k,'-->',v)
