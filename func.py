4ec212feca6956a76904e9aab191d4e211b9db75
Sujit901 <sharmasujit901@gmail.com>
2021-03-21 07:22:07 +0545

    change

--- func.py
+++ func.py
@@ -46,3 +46,53 @@
 #y = sorted(x)
 #y = x.sort()
 #print(x, y)
+
+
+# class Info:
+#     name = "Sujit"
+#     address = "Butwal"
+
+# obj = Info()
+
+# print(type(obj))
+
+# print(obj.name)
+# obj.address = "Kathmandu"
+# print(obj.address)
+# del obj.address
+# print(obj.address)
+
+
+# class Info:
+#     address = "Butwal"
+
+#     def __init__(self, name):
+#         #print(type(self))
+#         self.name = name
+#         print("name", name)
+
+# obj = Info("Sujit")
+# obj.address = "Bhairahawa"
+# print(obj.name, obj.address)
+
+# obj1 = Info("Sudip")
+# print(obj1.name, obj1.address)
+
+
+
+# class SumOfTwo:
+#     def __init__(self, a, b):
+#         self.a = a
+#         self.b = b
+    
+#     def add(self):
+#         s = self.a + self.b
+#         print("sum of two", s)
+
+# obj = SumOfTwo(10, 20)
+# print("obj.a", obj.a)
+# print("obj.b", obj.b)
+# obj.add()
+
+
+
